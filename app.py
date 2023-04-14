2b1adc10bf9d76e2ecde0ec80cab7e541871a777
TheNameisSea <chihaido06@gmail.com>
2023-04-14 12:17:16 +0700

    edited

--- app.py
+++ app.py
@@ -69,12 +69,22 @@ id2label = {0: 'Bạc lá',
             2: 'Bạc chuỳ lá',
             3: 'Cháy lúa',
             4: 'Đốm nâu',
-            5: 'Chết lá non',
+            5: 'Sâu đục thân lúa',
             6: 'Sương mai',
             7: 'Bọ gai',
             8: 'Bình thường',
             9: 'Vi-rút Tungro'}
 
+bacla = "Xử lý bệnh bạc lá:\n Sử dụng các thuốc trong danh mục thuốc BVTV được phép sử dụng ở Việt Nam có chứa các hoạt chất Bismerthiazol, Copper hydroxide, Oxolinic acid, Thiodiazole zinc, Thiodiazole copper,… để phun.\nGiai đoạn lúa đòng – trổ – chín, bà con cần theo dõi chặt chẽ diễn biến của thời tiết tiến hành phun trước và sau mưa giông bằng các thuốc bảo vệ thực vật có chứa hoạt chất nêu trên theo nguyên tắc 4 đúng và theo hướng dẫn sử dụng trên bao bì để ngăn chặn bệnh lây lan trên diện rộng."
+domsocla = "Xử lý bệnh đốm sọc lá:\nKhông bón quá nhiều đạm, bón đạm muộn và kéo dài; Chú ý kết hợp giữa bón đạm với phân chuồng, lân, kali.\nKhi phát hiện thấy ruộng chớm bị bệnh cần giữ mực nước từ 3 - 5 cm, tuyệt đối không được bón các loại phân hóa học, phân bón qua lá và thuốc kích thích sinh trưởng.\n Sử dụng các loại thuốc hoá học để phun phòng trừ như: Totan 200WP, Starwiner 20WP, Novaba 68WP, Xanthomix 20WP, PN - Balacide 32 WP, Starner 20 WP,... pha và phun theo hướng dẫn trên vỏ bao bì." 
+bacchuyla = "Xử lý bệnh bạc chuỳ lá:\nChọn giống lúa ít nhiễm bệnh, làm đất kỹ, giải độc hữu cơ, sạ thưa, bón phân cân đối NPK, bón đầy đủ canxi, silic, kali, không để nước ngập sâu kéo dài.\n Vào giai đoạn cuối đẻ nhánh, nuôi đòng nếu xuất hiện bệnh Cháy bìa lá do vi khuẩn có thể sử dụng Agrilife 100SL với liều 30ml/25 lít.\nVào giai đoạn lúa trổ đến chín: phun thuốc 25ml Agrilife 100SL +25ml Keviar 325SC/25 lít 2 lần: lúc lúa trổ lẹt xẹt và lúc lúa trổ đều."
+chaylua = "Xử lý bệnh cháy lúa:\nTrước khi trồng vụ mới thì tiến hành cải tạo và vệ sinh sạch đồng ruộng để tránh mầm khuẩn gây bệnh.\nBón phân cân đối dựa trên màu lá lúa, chú ý giữ mực nước trong ruộng thích hợp từ 5-10cm so với cây lúa.\nKhi mới phát hiện bệnh, nên rút nước trong ruộng ra rồi tiến hành rải vôi với liều lượng 10 – 20kg/1.000m2.\nSử dụng thuốc đặc trị bệnh cháy lá lúa khi cần thiết, sử dụng theo hướng dẫn của nhà sản xuất trên bao bì."
+domnau = "Xử lý bệnh đốm nâu:\nXử lý giống trước khi gieo bằng nước nóng 54 độ C hoặc bằng cách dùng một trong các loại thuốc trừ bệnh như Carban 50SC, Vicarben 50HP... pha nồng độ 3/1.000 ngâm giống 24-36 giờ sau đó vớt ra đãi sạch rồi đem ủ bình thường.\nSử dụng một trong các loại thuốc như: Kacie 250EC, Golcol 20SL, Supercin 20EC/40EC/80EC, Carbenzim 500FL, Tilt Super 300EC, Viroval 50BTN, Workup 9SL... để phun xịt. Về liều lượng và cách sử dụng nên đọc kỹ hướng dẫn của nhà sản xuất có in sẵn trên bao bì."
+sauducthanlua = "Xử lý bệnh sâu đục thân lúa:\n Sử dụng sản phẩm TT Checker 270SC, liều lượng 40ml/bình 25L.\nBổ sung thêm cho cây lúa chất điều hòa sinh trưởng như Plastimula 1SL với liều lượng 30ml/bình 25L ở các thời kỳ quan trọng như: xử lý giống, đẻ nhánh, làm đòng."
+suongmai = "Xử lý bệnh sương mai:\nSử dụng chế phẩm sinh học trừ bệnh RV04 với cơ chế tác động kép của nấm đối kháng và Enzyme kích kháng cây trồng tiêu diệt, cô lập các vết bệnh do nấm và vi khuẩn tấn công, ngăn chặn kịp thời sự lây lan của mầm bệnh.\nDựa vào điều kiện thời tiết mà nên tham khảo sử dụng để phòng bệnh từ đầu."
+bogai = "Xử lý bọ gai\nLàm sạch cỏ dại trong ruộng và bờ bao.\nDiệt trừ sâu non trên mạ mùa sắp cấy bằng cách ngắt bỏ các lá bị hại có bọ gai.\nDùng thuốc trừ sâu nhóm lân hữu cơ, Carbamate hoặc Cúc tổng hợp đều có thể diệt được bọ gai trưởng thành và ấu trùng."
+tungro = "Xử lý virus Tungro:\nPhun các loại thuốc trừ sâu có gốc buprofezin hay pymetrozine ở thời điểm ngày thứ 15 và ngày thứ 30 sau khi cấy có thể đạt hiệu quả nếu được thực hiện đúng thời gian. Các loài cây quanh cánh đồng cũng cần được phun các loại thuốc trừ sâu nêu trên.\nKhông nên sử dụng các sản phẩm thuốc trừ sâu có gốc chlorpyriphos, lamda cyhalothrin hay các công thức kết hợp các chất pyrethroid tổng hợp vì các loài sâu rầy đã phần nào đề kháng được các loại thuốc ấy. "
+binhthuong = "Lúa không bị bệnh hoặc không bị bệnh do virus / vi khuẩn / nấm"
 # Params
 params = {
     'model': 'efficientnet_b3',
@@ -158,10 +168,19 @@ def load_image(image_file):
 st.set_page_config(page_title="Rice Disease Classification", page_icon="🔬", layout="centered", initial_sidebar_state="expanded", menu_items=None) 
 
 # Page Title
+luu_y = [
+    'Sản phẩm chỉ sử dụng được cho lúa, xin vui lòng không nhập ảnh của các vật khác vào app',
+    'Tất cả các dự đoán dựa trên ảnh của các vật ngoài lúa đều không đáng tin',
+    'Chỉ có tác dụng với ảnh chụp từ điện thoại (tỉ lệ 4:3, độ phân giải 1440x1080)'
+]
+
 st.write("""
 # Bác Sĩ Lúa
 Chẩn đoán bệnh lúa dựa trên hình ảnh 
 """)
+st.write(
+    '[Lưu ý:]\n' + luu_y 
+)
          
 st.sidebar.header('Tải ảnh lên để bắt đầu chẩn đoán')
 uploaded_files = st.sidebar.file_uploader("Tải ảnh", accept_multiple_files=True)
@@ -242,27 +261,25 @@ if uploaded_files is not None:
             st.write(test_df)
             for i in label_col.unique():
                 if i == 0:
-                    st.write("https://vaas.vn/kienthuc/Caylua/06/14_benhbachla.htm")
+                    st.write(bacla)
                 elif i == 1:
-                    st.write("https://vaas.vn/kienthuc/Caylua/06/26_phongtrusaubenh.htm")
+                    st.write(domsocla)
                 elif i == 2:
-                    st.write("https://vaas.vn/kienthuc/Caylua/06/14_benhbachla.htm")
+                    st.write(bacchuyla)
                 elif i == 3:
-                    st.write("https://vaas.vn/kienthuc/Caylua/06/26_phongtrusaubenh.htm")
+                    st.write(chaylua)
                 elif i == 4:
-                    st.write("https://vaas.vn/kienthuc/Caylua/06/26_phongtrusaubenh.htm")
+                    st.write(domnau)
                 elif i == 5:
-                    st.write("https://vaas.vn/kienthuc/Caylua/06/01_sauducthan.htm")
+                    st.write(sauducthanlua)
                 elif i == 6:
-                    st.write("https://www.fao.org.vn/sau-benh/suong-mai/")
+                    st.write(suongmai)
                 elif i == 7:
-                    st.write("https://vaas.vn/kienthuc/Caylua/06/08_saugai.htm")
+                    st.write(bogai)
                 elif i == 8:
-                    st.write("")
+                    st.write(binhthuong)
                 elif i == 9:
-                    st.write("https://vaas.vn/kienthuc/Caylua/06/17_benhlunxoanla.htm")
-
-                    
+                    st.write(tungro)     
 
         with img_col:
             for img_path in os.listdir(upload_path):
